9b6bf892b2e920c8aa8b31920008cf57fc6f3fa0
C <calums88@gmail.com>
2023-04-05 20:25:52 +0100

    day3 homework

---
+++ src/python_functions_practice.py
@@ -0,0 +1,35 @@
+def return_10():
+    return 10
+
+def add(first_number, second_number):
+    return first_number + second_number
+
+def subtract(first_number, second_number):
+    return first_number - second_number
+
+def multiply(first_number, second_number):
+    return first_number * second_number
+
+def divide(first_number, second_number):
+    return first_number / second_number
+
+def length_of_string(string):
+    return len(string)
+
+def join_string(string_number_1, string_number_2):
+    return string_number_1 + string_number_2 
+
+def add_string_as_number(string_1, string_2):
+    return int(string_1) + int(string_2)
+
+
+def number_to_full_month_name(month_number):
+    months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    return months[month_number-1]
+
+def number_to_short_month_name(short_month_number):
+    short_months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    return short_months[short_month_number, 0]
+
+
+
